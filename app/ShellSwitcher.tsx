b3e844ed77b036a7211a5901c52a30781c8aecae
johnnyrusso <jrusso4188@gmail.com>
2026-03-27 19:12:27 -0400

    Fix 'usePathname is not defined' runtime error: Restore missing 'use client' and imports in ShellSwitcher.tsx.

--- app/ShellSwitcher.tsx
+++ app/ShellSwitcher.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import DiscoveryShell from '@/components/DiscoveryShell';
 import { DiscoveryItem, UIConfig } from '@/types/domain';
 
 interface ShellSwitcherProps {
